8ce342ea7b237bda79e2ad2f015aca80ee264703
Chance Magno and Janek Brandt <chance@>
2017-05-09 10:13:54 -0700

    add materialize

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
+
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterializeModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
